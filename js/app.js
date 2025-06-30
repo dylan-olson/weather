55ddf411b2f254c3c91a265f6b208da46e73bd52
Dylan Olson <olsondylan15@gmail.com>
2025-06-30 14:25:52 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up */
 const apiKey = "80701dba075f06003a038ffda4045d08";
 
 // set zipcode for this method 
-const inputZip = "90036";
+const inputZip = "38135";
 
 // doc ready 
 $(document).ready(function() {
